2b6bb0e8e57d96244b698e98c0a8e5da28257674
Kate Sv <radistkatya@gmail.com>
2021-11-10 13:53:07 -0800

    app setup

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const {STRING} = Sequelize
+const {STRING, ENUM} = Sequelize
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_departments_db')
 
 const User = conn.define('user', {
@@ -10,6 +10,7 @@ const User = conn.define('user', {
   }
 })
 
+
 const Department = conn.define('department', {
   name: {
     type: STRING,
@@ -18,8 +19,44 @@ const Department = conn.define('department', {
   }
 })
 
-Department.belongsTo(User)
-// User.hasMany(Department) the same as previous row
+const School = conn.define('schools', {
+  name: {
+    type: STRING,
+    allowNull: false,
+    unique: true
+  }
+})
+
+const Degree = conn.define('degree', {
+  level: {
+    type: ENUM('Masters', 'Bachelors', 'PhD')
+  }
+})
+
+Degree.belongsTo(User)
+Degree.belongsTo(School)
+
+Department.findWithManagers = ()=>{
+  return Department.findAll({
+    include: [{model: User, as: 'manager'}],
+    order: [
+      ['name']
+    ]
+  })
+}
+
+Department.prototype.isManaged = function (){
+  return !!this.managerId
+}
+
+Department.beforeSave(department => {
+  if(department.managerId ===""){
+    department.managerId = null
+  }
+})
+
+Department.belongsTo(User, {as: 'manager'})
+User.hasMany(Department, {foreignKey: 'managerId'})// the same as previous row, but the other way
 
 const syncAndSeed = async()=>{
   await conn.sync({force: true})
@@ -30,16 +67,30 @@ const syncAndSeed = async()=>{
   const [hr, engeneering, marketing] = await Promise.all(
     ['hr', 'engeneering', 'marketing'].map(name => Department.create({ name }))
   )
-  engeneering.userId = lucy.id;
-  marketing.userId = lucy.id;
+  engeneering.managerId = lucy.id;
+  marketing.managerId = lucy.id;
   await Promise.all([engeneering.save(), marketing.save()])
 
+  const [mit, caltech, lsu] = await Promise.all(
+    ['MIT', 'CalTech', 'LSU'].map(name => School.create({name}))
+  )
+  
+  await Promise.all([
+    Degree.create({ userId: moe.id, schoolId: mit.id, level: 'Bachelors'}),
+    Degree.create({ userId: lucy.id, schoolId: caltech.id, level: 'PhD'}),
+    Degree.create({ userId: lucy.id, schoolId: mit.id, level: 'Masters'}),
+    Degree.create({ userId: moe.id, schoolId: lsu.id}),
+  
+  ])
 }
+
 
 module.exports ={
   syncAndSeed,
   models: {
     User, 
-    Department
+    Department, 
+    School, 
+    Degree
   }
 }
--- server.js
+++ server.js
@@ -1,8 +1,107 @@
-const {syncAndSeed} = require('./db')
+const {syncAndSeed, models: { User, Department, School, Degree}} = require('./db')
+const express =  require('express')
+const app = express()
+
+app.use(require('method-override')('_method'));
+app.use(express.urlencoded({extended: false}))
+
+app.put ('/department/:id', async(req, res, next)=>{
+  try{
+    const department = await Department.findByPk(req.params.id)
+    await department.update(req.body)
+    res.redirect('/')
+  }
+  catch(ex){
+    next(ex)
+  }
+})
+
+
+app.get('/', async(req, res, next)=>{
+  try{
+    const [users, departments, degrees] = await Promise.all([
+      User.findAll({
+        include: [Department]
+      }),
+      Department.findWithManagers(),
+      Degree.findAll({
+        include: [School, User]
+      })
+    ])
+    res.send(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <title>Acme Corp Managment</title>
+      </head>
+      <body>
+      <div>
+        <h2>Deparments</h2>
+        <ul>
+        ${departments.map(department =>`
+          <li>
+          ${department.name}
+          ${department.isManaged()}
+          <form method ='POST' action='/department/${department.id}?_method=PUT'>
+          <select name ='managerId'>
+          <option value="" >-- not managed --</option>
+          ${
+            users.map (user =>`
+            <option value='${user.id}'${ user.id ===department.managerId ? 'selected = "selected"': ''}>${user.name}</option>
+            `).join('')
+          }
+          </select>
+          <button>Save</button>
+          </form>
+
+          </li>
+        `).join('')}
+        </ul>
+      </div>
+      <div>
+      <h2>Users</h2>
+      <ul>
+        ${users.map(user =>`
+          <li>
+          ${user.name}
+          <ul>
+          ${ user.departments.map (department => `
+          <li>
+          ${department.name}
+          </li>
+          
+          `).join('')}
+          </ul>
+          </li>
+        `).join('')}
+        </ul>
+      </div>
+      <div>
+      <h2>Degrees</h2>
+      <ul>
+        ${degrees.map(degree =>`
+          <li>
+          ${degree.user.name} attanded ${degree.school.name} (${degree.level || 'no degree'})
+          </li>
+          `).join('')
+        }
+          </ul>
+      </div>
+      </body>
+      </html>
+      `
+    )
+  }
+  catch(ex){
+    next(ex)
+  }
+})
 
 const init = async()=>{
   try{
     await syncAndSeed()
+    const port =process.env.PORT || 3000
+    app.listen(port, ()=> console.log(`listening on port ${port}`))
   }
   catch(ex){
     console.log(ex)
